57dae528efcb7af63c618f2c9ae89dba9f428610
Amir Samary <amirsamary@gmail.com>
2020-03-11 16:24:30 -0400

    Renaming OEE folder to image-OEE

--- build.sh
+++ build.sh
@@ -6,9 +6,13 @@ source ./buildtools.sh
 
 OEE_IMAGE_NAME=intersystemsdc/irisdemo-demo-oee:OEE-${DOCKER_TAG}
 DISPATCH_IMAGE_NAME=intersystemsdc/irisdemo-demo-oee:DISPATCH-${DOCKER_TAG}
+OPCUA_SAMPLER_IMAGE_NAME=intersystemsdc/irisdemo-demo-oee:opcua-sampler-${DOCKER_TAG}
 
-docker build -t $OEE_IMAGE_NAME ./OEE
-exit_if_error "Could not build OEE image"
+docker build -t $OPCUA_SAMPLER_IMAGE_NAME ./image-opcua-sampler
+exit_if_error "Could not build OPCUA SAMPLER image"
 
-docker build -t $DISPATCH_IMAGE_NAME ./DISPATCH
-exit_if_error "Could not build DISPATCH image"
+# docker build -t $OEE_IMAGE_NAME ./OEE
+# exit_if_error "Could not build OEE image"
+
+# docker build -t $DISPATCH_IMAGE_NAME ./DISPATCH
+# exit_if_error "Could not build DISPATCH image"
